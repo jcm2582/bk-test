525fd9e33abe991f28829dc5b73e82b3da8bdec9
Joel Mora <jcm2582@unm.edu>
2018-07-14 19:23:45 -0600

    PHP Tests

--- index.php
+++ index.php
@@ -1,25 +1,33 @@
 <?php
 
-$request = new HttpRequest();
-$request->setUrl('https://freetrial1566893.beekeeper.io/api/2//status');
-$request->setMethod(HTTP_METH_GET);
+$curl = curl_init();
 
-$request->setHeaders(array(
-  'postman-token' => '4ecc2970-92b9-442f-85e2-0435bad927f5',
-  'cache-control' => 'no-cache',
-  'content-type' => 'application/json',
-  'accept' => 'application/json',
-  'authorization' => 'Token 47e6a7a2-a0d0-468f-92c1-7f45f0f0bdff'
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://freetrial1566893.beekeeper.io/api/2//status",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_POSTFIELDS => "echo 'hello world'",
+  CURLOPT_HTTPHEADER => array(
+    "accept: application/json",
+    "authorization: Token 47e6a7a2-a0d0-468f-92c1-7f45f0f0bdff",
+    "cache-control: no-cache",
+    "content-type: application/json",
+    "postman-token: b961ea01-446c-5a1d-eaed-13fe73384b92"
+  ),
 ));
 
-$request->setBody('echo 'hello world'');
+$response = curl_exec($curl);
+$err = curl_error($curl);
 
-try {
-  $response = $request->send();
+curl_close($curl);
 
-  echo $response->getBody();
-} catch (HttpException $ex) {
-  echo $ex;
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
 }
-
 ?>
